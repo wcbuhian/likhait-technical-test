9b39a66641774804e90f65825684281bae254d9e
William Buhian <william.buhian1@gmail.com>
2026-03-09 14:05:21 +0800

    add backend validation logic for Category model

--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,3 +1,14 @@
 class Category < ApplicationRecord
   has_many :expenses, dependent: :destroy
+  validates :name, presence: true
+
+  validate :emoji_is_single_char, if: -> { emoji.present? }
+
+  private
+
+  def emoji_is_single_char
+    if emoji.grapheme_clusters.length != 1
+      errors.add(:emoji, "must be a single character")
+    end
+  end
 end
