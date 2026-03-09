1dce3b4062dab6dfe0060a12f35c49e718175108
William Buhian <william.buhian1@gmail.com>
2026-03-09 16:06:45 +0800

    backend model validation

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ApplicationRecord
   belongs_to :category
+  validates :description, :amount, :date, presence: true
+  validates :date, comparison: { less_than_or_equal_to: -> {Date.current} }
 end
